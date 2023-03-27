c10fdf81710a8a89456e8497f916965608fadc28
BrayanGuerreroXD <bg7047653@gmail.com>
2023-03-27 15:32:19 -0500

    Finish project v1

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { getStudent } from "../controllers/students.controller";
-import { createRegister } from "../controllers/registers.controller";
+import { getStudent } from "../controllers/students.controller.js";
+import { createRegister } from "../controllers/registers.controller.js";
 
 const router = Router();
 

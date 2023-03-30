b65da61f37797228ea40c3da5c0b82a9a90c6d29
BrayanGuerreroXD <bg7047653@gmail.com>
2023-03-30 08:14:52 -0500

    Add cros dependency

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from "./routes/api.routes.js";
 
+const corsOptions = {
+  origin: 'http://localhost:5173'
+};
+
 const app = express();
+
+// Configura CORS con opciones específicas
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
